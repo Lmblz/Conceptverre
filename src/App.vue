9b4fe4de4b1e799475f0544d08ef3ba1daea7df2
lambo <lambolez.th@gmail.com>
2023-01-25 17:23:57 +0100

    Styles header homepage

--- src/App.vue
+++ src/App.vue
@@ -14,4 +14,70 @@
 </script>
 
 <style lang="scss">
+    body {
+        margin: 0;
+    }
+
+    @font-face {
+        font-family: 'Silk Serif Regular Italic';
+        src: url('./assets/fonts/Silk_Serif_Regular_Italic.otf') format('opentype');
+    }
+    $SilkRegIt : 'Silk Serif Regular Italic', Arial, Helvetica, sans-serif;
+
+    @font-face {
+        font-family: 'Silk Serif Regular';
+        src: url('./assets/fonts/Silk_Serif_Regular.otf') format('opentype');
+    }
+    $SilkReg : 'Silk Serif Regular', Arial, Helvetica, sans-serif;
+
+    @font-face {
+        font-family: 'Darker Grotesque Medium';
+        src: url('./assets/fonts/DarkerGrotesque-Medium.ttf') format('truetype');
+    }
+    $DGMed: 'Darker Grotesque Medium', Arial, Helvetica, sans-serif;
+
+
+
+
+
+    #app {
+
+        h1 {
+            font-family: $SilkReg;
+            font-size: 7.5rem;
+            margin: 0
+        }
+
+        p, a, span, button, li {
+            font-family: $DGMed;
+            font-size: 2rem;
+            margin: 0;
+        }
+
+        button {
+            padding: 0.5rem 2rem 0.9rem 2rem;
+            line-height: 1;
+
+            &.btn-white {
+                background: transparent;
+                border: solid 1px white;
+                text-transform: uppercase;
+                color: white;
+            }
+        }
+
+
+        nav {
+            position: sticky;
+            top: 0;
+            height: 0;
+            display: flex;
+            z-index: 10;
+            max-width: 85%;
+            width: 100%;
+            margin: auto;
+            color: white;
+            justify-content: space-between;
+        }
+    }
 </style>
--- src/components/HomepageHeader.vue
+++ src/components/HomepageHeader.vue
@@ -1,8 +1,15 @@
 <template>
-    <header>
+    <header class="header-homepage">
         <picture class="header-homepage__background">
-            
+            <source srcset="../assets/homepage/header.webp" type="image/webp" />
+            <img src="../assets/homepage/header.jpg" />
         </picture>
+        <div class="header-homepage__description">
+            <h1>Donnez <i>vie</i> à votre <i>intérieur</i></h1>
+            <p><b>Concept Verre</b> voit le jour en 2007, quintessence d'un travail passionnant et passionné.</p>
+            <p>Dans notre atelier basé à Nice, nous concevons des <b>luminaires d'intérieur au design épuré</b></p>
+            <button class="header-homepage__description__button btn-white">Découvrir</button>
+        </div>
     </header>
 </template>
 
@@ -11,3 +18,37 @@
         name: 'HomepageHeader'
     }
 </script>
+
+<style lang="scss" scoped>
+    #app {
+        .header-homepage {
+            position: relative;
+            text-align: center;
+
+            &__background {
+                height: auto;
+                width: auto;
+                display: block;
+
+                img {
+                    object-fit: cover;
+                    max-height: 100vh;
+                    height: 100%;
+                    width: 100%;
+                }   
+            }
+            &__description {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translateX(-50%) translateY(-50%);
+                color: white;
+                width: 100%;
+                
+                &__button {
+                    margin-top: 5rem;
+                }
+            }
+        }
+    }
+</style>
