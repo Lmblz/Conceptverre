54cfc910c7a933b13c4cd5eb73a53bd7428a47d4
lambo <lambolez.th@gmail.com>
2023-01-25 16:24:28 +0100

    HomepageView - NavBar - HomepageHeader

---
+++ src/components/HomepageHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <picture class="header-homepage__background">
+            
+        </picture>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'HomepageHeader'
+    }
+</script>
